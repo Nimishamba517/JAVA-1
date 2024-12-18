6460f825b7e0412e998d809a1a25ab09cfd2eba8
Nimishamba517 <nimishamba.23cs086@sode-edu.in>
2024-12-18 14:35:09 +0530

    Add files via upload

---
+++ Lab12.java
@@ -0,0 +1,31 @@
+class MyThread extends Thread{
+	public MyThread(String name){
+		super(name);
+		start();
+	}
+
+public void run(){
+	for(int i=1;i<=5;i++){
+		System.out.println("Child Thread"+i);
+		try{
+			Thread.sleep(1000);
+			}catch(InterruptedException e){
+				System.out.println("Child thread interrupted ");
+			}
+		}
+	}
+}
+public class Lab12{
+	public static void main(String[] args){
+		MyThread t1=new MyThread("ChildThread");
+
+for(int i=1;i<=5;i++){
+		System.out.println("Main Thread"+i);
+		try{
+			Thread.sleep(1000);
+			}catch(InterruptedException e){
+				System.out.println("Main thread interrupted ");
+			}
+		}
+	}
+}
